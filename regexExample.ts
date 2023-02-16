09c5bd34d2c5cde2ba7a8f2fdbd90797bccbd53e
Jon Garrison <jongarrison@gmail.com>
2023-02-16 08:23:37 -0800

    Including regex example

---
+++ regexExample.ts
@@ -0,0 +1,29 @@
+
+
+console.log("running reg ex examples...")
+
+
+const categoryTexts = [
+    "Some category text (123)",
+    "Important category (555)"
+]
+
+const nameMatchRegex = new RegExp("([^\(\)]+)");
+const numberMatchRegex = new RegExp("(\\d+)");
+
+for (let categoryText of categoryTexts) {
+
+    console.log(`Looking at category text: ${categoryText}`)
+    const nameMatch = nameMatchRegex.exec(categoryText)
+    if (nameMatch) {
+        console.log(`> matched name: ${nameMatch[0]}`)
+    }
+
+    const numberMatch = numberMatchRegex.exec(categoryText)
+    if (numberMatch) {
+        console.log(`> matched number: ${numberMatch[0]}`)
+    } else {
+        console.log(`> ${numberMatch}`)
+    }
+
+}
